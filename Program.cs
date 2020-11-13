f29620790d9a425c129410611fc1b1cfae2e8a40
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-13 09:53:54 -0600

    moose AMA

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
-namespace MagicMoose
+namespace EnthusiasticMoose
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Magic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+
+            // Ask a question
+            AskMoose("Ask me a question!");
+        }
+
+        static void AskMoose(string message)
+        {
+            Console.WriteLine($@"
+                                                _.--^^^--,
+                                                .'          `\
+            .-^^^^^^-.                      .'              |
+            /          '.                   /            .-._/
+            |             `.                |             |
+            \              \          .-._ |          _   \
+            `^^'-.         \_.-.     \   `          ( \__/
+                    |             )     '=.       .,   \
+                /             (         \     /  \  /
+                /`               `\        |   /    `'
+                '..-`\        _.-. `\ _.__/   .=.
+                    |  _    / \  '.-`    `-.'  /
+                    \_/ |  |   './ _     _  \.'
+                        '-'    | /       \ |
+                                |  .-. .-.  |
+                                \ / o| |o \ /
+                                |   / \   |    {message}
+                                / `^`   `^` \
+                                /             \
+                                | '._.'         \
+                                |  /             |
+                                \ |             |
+                                ||    _    _   /
+                                /|\  (_\  /_) /
+                                \ \'._  ` '_.'
+                                `^^` `^^^`
+            ");
+            Console.Write(":");
+            string answer = Console.ReadLine();
         }
     }
 }
